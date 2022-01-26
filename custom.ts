d8e617200b03e5346a35b1b00913b8788ce4fca0
PriyanshSingh-2305 <83878648+PriyanshSingh-2305@users.noreply.github.com>
2022-01-26 11:46:57 +0530

    Update main.blocks, main.ts, custom.ts

--- custom.ts
+++ custom.ts
@@ -1,55 +1,64 @@
+enum Motor {
+    //% block="A"
+    A = 0x1,
+    //% block="B"
+    B = 0x2,
+}
 
-/**
-* Use this file to define custom functions and blocks.
-* Read more at https://makecode.microbit.org/blocks/custom
-*/
+enum Dir {
+    //% block="Forward"
+    forward = 0x1,
+    //% block="Backward"
+    backward = 0x2,
+}
 
-/**
- * Custom blocks
- */
-//% weight=100 color=#2600d5 icon=""
+let PWMA = AnalogPin.P0;
+let AIN1 = DigitalPin.P1;
+let AIN2 = DigitalPin.P8;
+let PWMB = AnalogPin.P2;
+let BIN1 = DigitalPin.P12;
+let BIN2 = DigitalPin.P16;
+
+//% weight=20 color=#3333FF icon="\uf1b9"
 namespace MotorDriver {
     /**
-     * TODO: describe your function here
-     * @param n describe parameter here, eg: 5
-     * @param s describe parameter here, eg: "Hello"
-     * @param e describe parameter here
-     */
-    //% block
-    export function goForward(speed: number): void {
-        pins.analogWritePin(AnalogPin.P0, speed)
-        pins.digitalWritePin(DigitalPin.P1, 0)
-        pins.digitalWritePin(DigitalPin.P8, 1)
-        pins.analogWritePin(AnalogPin.P2, speed)
-        pins.digitalWritePin(DigitalPin.P12, 0)
-        pins.digitalWritePin(DigitalPin.P16, 1)
+     * Motor Run
+     * @param speed [0-16] speed of Motor; eg: 10, 0, 16
+    */
+    //% blockId=MotorDriver_MotorRun block="Motor %m|index %index|speed %speed"
+    //% weight=100
+    //% speed.min=0 speed.max=16
+    export function MotorRun(m: Motor, index: Dir, speed: number): void {
+        speed = speed * 64 - 1; // map 0 to 1023
+
+        if (m == Motor.A) {
+            pins.analogWritePin(PWMA, speed)
+            if (index == Dir.forward) {
+                pins.digitalWritePin(AIN1, 0)
+                pins.digitalWritePin(AIN2, 1)
+            } else {
+                pins.digitalWritePin(AIN1, 1)
+                pins.digitalWritePin(AIN2, 0)
+            }
+        } else {
+            pins.analogWritePin(PWMB, speed)
+            if (index == Dir.forward) {
+                pins.digitalWritePin(BIN1, 0)
+                pins.digitalWritePin(BIN2, 1)
+            } else {
+                pins.digitalWritePin(BIN1, 1)
+                pins.digitalWritePin(BIN2, 0)
+            }
+        }
     }
 
-    /**
-     * TODO: describe your function here
-     * @param value describe value here, eg: 5
-     */
-    //% block
-    export function goBackward(speed: number): void {
-        pins.analogWritePin(AnalogPin.P0, speed)
-        pins.digitalWritePin(DigitalPin.P1, 1)
-        pins.digitalWritePin(DigitalPin.P8, 0)
-        pins.analogWritePin(AnalogPin.P2, speed)
-        pins.digitalWritePin(DigitalPin.P12, 1)
-        pins.digitalWritePin(DigitalPin.P16, 0)
-    }
-
-    /**
-     * TODO: describe your function here
-     * @param value describe value here, eg: 5
-     */
-    //% block
-    export function Stop(): void {
-        pins.analogWritePin(AnalogPin.P0, 0)
-        pins.digitalWritePin(DigitalPin.P1, 0)
-        pins.digitalWritePin(DigitalPin.P8, 0)
-        pins.analogWritePin(AnalogPin.P2, 0)
-        pins.digitalWritePin(DigitalPin.P12, 0)
-        pins.digitalWritePin(DigitalPin.P16, 0)
+    //% blockId=MotorStop
+    //% block="Motor %Motor| Stop"
+    //% weight=90
+    export function MotorStop(m: Motor): void {
+        if (m == Motor.A)
+            pins.analogWritePin(PWMA, 0)
+        else
+            pins.analogWritePin(PWMB, 0)
     }
 }
--- main.ts
+++ main.ts
@@ -1,3 +1,3 @@
 basic.forever(function () {
-	
+    MotorDriver.MotorRun(Motor.A, Dir.forward, 10)
 })
